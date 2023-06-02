6734a2a37672c063da8700c214d073bbfc3a84c7
Alison Tahiri <alison.tahiri@gmail.com>
2023-06-02 16:21:17 +0200

    add theme toggle functionality

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "preact/hooks";
+
+export default function ThemeToggle() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light");
+
+  const handleClick = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      document.documentElement.removeAttribute("data-theme");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <button onClick={handleClick}>{theme === "light" ? "🌙" : "🌞"}</button>
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,astro}"],
+  theme: {
+    extend: {
+      colors: {
+        accent: {
+          1: "hsl(var(--color-accent1) / <alpha-value>)",
+          2: "hsl(var(--color-accent2) / <alpha-value>)",
+        },
+
+        bgk: "hsl(var(--color-bkg) / <alpha-value>)",
+        content: "hsl(var(--color-content) / <alpha-value>)",
+      },
+    },
+  },
+  darkMode: "media",
+  plugins: [],
+};
